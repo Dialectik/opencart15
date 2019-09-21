35d605b90cb28c291c5b3f97f6350586b0d2d434
Dialectik <dialectik@i.ua>
2019-09-21 12:28:53 +0300

    First commit

---
+++ admin/language/russian/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer'] = '<a href="http://opencart-russia.ru" target="_blank">Русское сообщество OpenCart</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://forum.opencart-russia.ru" target="_blank">Форум поддержки</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://shop.opencart-russia.ru" target="_blank">Магазин дополнений</a>';
+?>
